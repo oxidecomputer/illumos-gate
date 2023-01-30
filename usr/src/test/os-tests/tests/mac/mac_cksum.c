8ffc90b3f611b10f062ebd187c40d4ca7b04cd4e
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2023-01-30 14:58:34 -0800

    stlouis#292 Want IPv6 Fast Path stlouis#543 Want IPv6 LSO Portions contributed by: Patrick Mooney <pmooney@pfmooney.com> Portions contributed by: Kyle Simpson <kyle@oxide.computer>
    This commit allows DLS bypass to be set for IPv6 ills. Beyond removing
    the checks and assertions that prevent this from happening, the
    following has been done to the fanout machinery.
    
    - Add the ability to hash IPv6/{UDP/TCP} packets.
    - Extract the packet protocol from the next-header IPv6 header field.
    - IPv6 header length calculation (trivial as v6 headers are not variable
      size).
    - Add an TCP6 and UDP6 soft rings to mac_soft_ring_set_s
    - Replace mac_client_impl_s members mci_direct_rx_fn and
      mci_direct_rx_arg with v4/v6 specific variants.
    - Various bits of plumbing to have both dls bypass and dls direct
      plumbed with v4/v6 specific upcalls and arguments.
    
    This patch also reverts #12731 making LSO available for IPv6, and adds
    the necessary LSO emulation for IPv6. There are a number of tests in
    illumos issues #12731 and #12676 that I would like to revive. These
    tests are not currently in a runnable for reasons not related to this
    patch. As we work toward upstreaming this work I will work on getting
    these tests running on gate.
    
    LSO emulation has been tested by creating two VNICs atop a single
    physical data link, placing them in zones and running iperf in TCP mode
    over IPv6 between each zone. Simultaneous IPv4 and IPv6 iperf sessions
    were also tested. Dtrace was used to verify that mac_sw_lso was being
    exercised.
    
    With this patch, I see a significant performance increase that is on par
    with IPv4 on a 10 gbps link for a single iperf3 session.
    
    - before:       5.95 Gbits/sec
    - after:        9.11 Gbits/sec
    
    This patch has also been tested with simultaneous v4/v6 iperf streams
    and splits bandwidth almost evenly between the two at
    
    - IPv6:         4.54 Gbits/sec
    - IPv4:         4.76 Gbits/sec
    
    Both the DLS bypass and LSO code paths have been tested on debug and
    release builds.
    
    Change-Id: Ic7d4952f58d8d0b506abf9c8124a6e4e00a55a07

--- usr/src/test/os-tests/tests/mac/mac_cksum.c
+++ usr/src/test/os-tests/tests/mac/mac_cksum.c
@@ -28,177 +28,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#include <unistd.h>
 #include <err.h>
 #include <errno.h>
-#include <strings.h>
-#include <netinet/in.h>
-#include <sys/mman.h>
-#include <sys/dlpi.h>
-#include <sys/types.h>
-#include <sys/types32.h>
-#include <sys/stat.h>
-#include <sys/sysmacros.h>
-#include <sys/ethernet.h>
 
-#include <libnvpair.h>
 #include <libktest.h>
 
-typedef struct snoop_pkt_hdr {
-	uint_t			sph_origlen;
-	uint_t			sph_msglen;
-	uint_t			sph_totlen;
-	uint_t			sph_drops;
-#if defined(_LP64)
-	struct timeval32	sph_timestamp;
-#else
-#error	"ktest is expected to be 64-bit for now"
-#endif
-} snoop_pkt_hdr_t;
-
-typedef struct snoop_file_hdr {
-	char		sfh_magic[8];
-	uint32_t	sfh_vers;
-	uint32_t	sfh_mac_type;
-} snoop_file_hdr_t;
-
-static const char snoop_magic[8] = "snoop\0\0\0";
-static const uint_t snoop_acceptable_vers = 2;
-
-typedef struct pkt_cap_iter {
-	int		pci_fd;
-	const char	*pci_base;
-	size_t		pci_map_sz;
-	size_t		pci_sz;
-	size_t		pci_offset;
-} pkt_cap_iter_t;
-
-static pkt_cap_iter_t *
-pkt_cap_open(int fd)
-{
-	struct stat info;
-	if (fstat(fd, &info) != 0) {
-		return (NULL);
-	}
-	if (info.st_size < sizeof (snoop_file_hdr_t)) {
-		errno = EINVAL;
-		return (NULL);
-	}
-
-	const size_t page_sz = (size_t)sysconf(_SC_PAGESIZE);
-	const size_t map_sz = P2ROUNDUP(info.st_size, page_sz);
-	void *map = mmap(NULL, map_sz, PROT_READ, MAP_PRIVATE, fd, 0);
-	if (map == NULL) {
-		return (NULL);
-	}
-
-	const snoop_file_hdr_t *hdr = (const snoop_file_hdr_t *)map;
-	if (bcmp(&hdr->sfh_magic, snoop_magic, sizeof (hdr->sfh_magic)) != 0 ||
-	    ntohl(hdr->sfh_vers) != snoop_acceptable_vers ||
-	    ntohl(hdr->sfh_mac_type) != DL_ETHER) {
-		(void) munmap(map, map_sz);
-		errno = EINVAL;
-		return (NULL);
-	}
-
-	struct pkt_cap_iter *iter = malloc(sizeof (struct pkt_cap_iter));
-	if (iter == NULL) {
-		(void) munmap(map, map_sz);
-		errno = ENOMEM;
-		return (NULL);
-	}
-
-	iter->pci_fd = fd;
-	iter->pci_base = (const char *)map;
-	iter->pci_map_sz = map_sz;
-	iter->pci_sz = info.st_size;
-	iter->pci_offset = sizeof (*hdr);
-
-	return (iter);
-}
-
-static void
-pkt_cap_close(pkt_cap_iter_t *iter)
-{
-	(void) munmap((void *)iter->pci_base, iter->pci_map_sz);
-	(void) close(iter->pci_fd);
-	free(iter);
-}
-
-static bool
-pkt_cap_next(pkt_cap_iter_t *iter, const void **pkt_buf, uint_t *sizep)
-{
-	size_t remain = iter->pci_sz - iter->pci_offset;
-
-	if (remain < sizeof (snoop_pkt_hdr_t)) {
-		return (false);
-	}
-
-	const snoop_pkt_hdr_t *hdr =
-	    (const snoop_pkt_hdr_t *)&iter->pci_base[iter->pci_offset];
-
-	const uint_t msg_sz = ntohl(hdr->sph_msglen);
-	const uint_t total_sz = ntohl(hdr->sph_totlen);
-	if (remain < total_sz || remain < msg_sz) {
-		return (false);
-	}
-
-	*pkt_buf = (const void *)&hdr[1];
-	*sizep = msg_sz;
-	iter->pci_offset += total_sz;
-	return (true);
-}
+#include "mac_ktest_common.h"
 
 static ktest_hdl_t *kthdl = NULL;
 const char *mac_cksum_cmd = "";
-
-struct payload_opts {
-	uint_t		po_padding;
-	boolean_t	po_cksum_partial;
-	boolean_t	po_cksum_full;
-	boolean_t	po_cksum_ipv4;
-	boolean_t	po_split_ether;
-	uint_t		po_split_manual;
-};
-
-static char *
-build_payload(const void *pkt_buf, uint_t pkt_sz,
-    const struct payload_opts *popts, size_t *payload_sz)
-{
-	nvlist_t *payload = fnvlist_alloc();
-	fnvlist_add_byte_array(payload, "pkt_bytes",
-	    (uchar_t *)pkt_buf, pkt_sz);
-	if (popts->po_padding) {
-		fnvlist_add_uint32(payload, "padding", popts->po_padding);
-	}
-	if (popts->po_cksum_partial) {
-		fnvlist_add_boolean(payload, "cksum_partial");
-	}
-	if (popts->po_cksum_full) {
-		fnvlist_add_boolean(payload, "cksum_full");
-	}
-	if (popts->po_cksum_ipv4) {
-		fnvlist_add_boolean(payload, "cksum_ipv4");
-	}
-
-	uint_t nsplit = 0;
-	uint32_t splits[2];
-	if (popts->po_split_ether) {
-		splits[nsplit++] = sizeof (struct ether_header);
-	}
-	if (popts->po_split_manual != 0) {
-		splits[nsplit++] = popts->po_split_manual;
-	}
-	if (nsplit > 0) {
-		fnvlist_add_uint32_array(payload, "cksum_splits", splits,
-		    nsplit);
-	}
-
-	char *packed = fnvlist_pack(payload, payload_sz);
-	nvlist_free(payload);
-
-	return (packed);
-}
 
 static void
 mac_cksum_usage(void)
@@ -303,8 +141,8 @@ main(int argc, char *argv[])
 			.krq_test = "mac_sw_cksum_test",
 		};
 		size_t payload_sz;
-		char *payload =
-		    build_payload(pkt_buf, pkt_sz, &popts, &payload_sz);
+		char *payload = build_payload(pkt_buf, pkt_sz, NULL, 0,
+		    &popts, &payload_sz);
 		req.krq_input = (uchar_t *)payload;
 		req.krq_input_len = (uint_t)payload_sz;
 
