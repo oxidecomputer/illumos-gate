9d4dbdc389e4f0c1d9327bfe31b468f206e706cc
Luqman Aden <luqman@oxide.computer>
2023-12-22 17:56:52 -0800

    First pass at genoa_ccx_init - adjust MSRs
    Change-Id: If58d5e9a5ad809f317dfe94c2ee963a9b600401a

--- usr/src/uts/oxide/genoa/genoa_ccx.c
+++ usr/src/uts/oxide/genoa/genoa_ccx.c
@@ -276,7 +276,6 @@ genoa_thread_feature_init(void)
 {
 	uint64_t v;
 	x86_chiprev_t chiprev = cpuid_getchiprev(CPU);
-	x86_uarchrev_t uarchrev = cpuid_getuarchrev(CPU);
 
 	v = rdmsr(MSR_AMD_CPUID_7_FEATURES);
 	v = AMD_CPUID_7_FEATURES_SET_RTM(v, 0);
@@ -293,10 +292,8 @@ genoa_thread_feature_init(void)
 	 * obtain entropy by other means.
 	 */
 	v = AMD_CPUID_7_FEATURES_SET_RDSEED(v, 0);
-	if (chiprev_matches(chiprev, X86_CHIPREV_AMD_GENOA_B0))
-		v = AMD_CPUID_7_FEATURES_SET_ERMS(v, 0);
-	else
-		v = AMD_CPUID_7_FEATURES_SET_ERMS(v, 1);
+
+	v = AMD_CPUID_7_FEATURES_SET_ERMS(v, 1);
 
 	wrmsr_and_test(MSR_AMD_CPUID_7_FEATURES, v);
 
@@ -315,32 +312,32 @@ genoa_thread_feature_init(void)
 	v = rdmsr(MSR_AMD_FEATURE_EXT2_EAX);
 	v = AMD_FEATURE_EXT2_EAX_SET_NULL_SELECTOR_CLEARS_BASE(v, 1);
 	if (genoa_ccx_set_undoc_fields &&
-	    (uarchrev_matches(uarchrev, X86_UARCHREV_AMD_ZEN3_B0) ||
-	    chiprev_at_least(chiprev, X86_CHIPREV_AMD_GENOA_B0))) {
-		v = AMD_FEATURE_EXT2_EAX_U_ZEN3_B0_SET_UNKNOWN_4(v, 0);
+	    (chiprev_matches(chiprev, X86_CHIPREV_AMD_GENOA_ANY))) {
+		v = AMD_FEATURE_EXT2_EAX_U_ZEN4_SET_UNKNOWN_4(v, 0);
 	}
 
 	wrmsr_and_test(MSR_AMD_FEATURE_EXT2_EAX, v);
 
-	if (uarchrev_at_least(uarchrev, X86_UARCHREV_AMD_ZEN3_B0)) {
-		v = rdmsr(MSR_AMD_STRUCT_EXT_FEAT_ID_EDX0_ECX0);
-		v = AMD_STRUCT_EXT_FEAT_ID_EDX0_ECX0_SET_FSRM(v, 1);
+	v = rdmsr(MSR_AMD_STRUCT_EXT_FEAT_ID_EDX0_ECX0);
+	v = AMD_STRUCT_EXT_FEAT_ID_EDX0_ECX0_SET_FSRM(v, 1);
 
-		wrmsr_and_test(MSR_AMD_STRUCT_EXT_FEAT_ID_EDX0_ECX0, v);
-	}
+	wrmsr_and_test(MSR_AMD_STRUCT_EXT_FEAT_ID_EDX0_ECX0, v);
+
+	v = rdmsr(MSR_AMD_OSVW_ID_LENGTH);
+	v = AMD_OSVW_ID_LENGTH_SET_LENGTH(v, 5);
+
+	wrmsr_and_test(MSR_AMD_OSVW_ID_LENGTH, v);
+
+	v = rdmsr(MSR_AMD_OSVW_STATUS);
+	v = AMD_OSVW_STATUS_SET_STATUS_BITS(v, 0);
+
+	wrmsr_and_test(MSR_AMD_OSVW_STATUS, v);
 }
 
 static void
 genoa_thread_uc_init(void)
 {
 	uint64_t v;
-	x86_chiprev_t chiprev = cpuid_getchiprev(CPU);
-
-	/*
-	 * The fields we modify in MCODE_CTL are reserved on A0.
-	 */
-	if (!chiprev_at_least(chiprev, X86_CHIPREV_AMD_GENOA_B0))
-		return;
 
 	v = rdmsr(MSR_AMD_MCODE_CTL);
 	v = AMD_MCODE_CTL_SET_REP_STOS_ST_THRESH(v,
@@ -355,15 +352,12 @@ static void
 genoa_core_ls_init(void)
 {
 	uint64_t v;
-	x86_chiprev_t chiprev = cpuid_getchiprev(CPU);
 
 	v = rdmsr(MSR_AMD_LS_CFG);
+	v = AMD_LS_CFG_SET_SPEC_LOCK_MAP_DIS(v, 0);
+	v = AMD_LS_CFG_U_ZEN4_SET_DIS_SPEC_WC_REQ(v, 1);
 	v = AMD_LS_CFG_SET_TEMP_LOCK_CONT_THRESH(v, 1);
 	v = AMD_LS_CFG_SET_ALLOW_NULL_SEL_BASE_LIMIT_UPD(v, 1);
-	v = AMD_LS_CFG_SET_SBEX_MISALIGNED_TLBMISS_MA1_FRC_MA2(v, 1);
-	if (chiprev_matches(chiprev, X86_CHIPREV_AMD_GENOA_A0)) {
-		v = AMD_LS_CFG_SET_SPEC_LOCK_MAP_DIS(v, 1);
-	}
 	/*
 	 * XXX Possible boot-time or per-thread/guest policy option.
 	 */
@@ -371,73 +365,24 @@ genoa_core_ls_init(void)
 
 	wrmsr_and_test(MSR_AMD_LS_CFG, v);
 
-	v = rdmsr(MSR_AMD_LS_CFG2);
-	if (chiprev_at_least(chiprev, X86_CHIPREV_AMD_GENOA_B0)) {
-		v = AMD_LS_CFG2_SET_DIS_ST_PIPE_COMP_BYP(v, 0);
-		v = AMD_LS_CFG2_SET_DIS_FAST_TPR_OPT(v, 0);
-		v = AMD_LS_CFG2_SET_HW_PF_ST_PIPE_PRIO_SEL(v, 3);
-	} else {
-		v = AMD_LS_CFG2_SET_DIS_ST_PIPE_COMP_BYP(v, 1);
-		v = AMD_LS_CFG2_SET_DIS_FAST_TPR_OPT(v, 1);
-		v = AMD_LS_CFG2_SET_HW_PF_ST_PIPE_PRIO_SEL(v, 1);
-	}
-
-	wrmsr_and_test(MSR_AMD_LS_CFG2, v);
-
 	v = rdmsr(MSR_AMD_LS_CFG3);
-	if (chiprev_at_least(chiprev, X86_CHIPREV_AMD_GENOA_B0) &&
-	    genoa_ccx_set_undoc_fields) {
-		v = AMD_LS_CFG3_SET_UNKNOWN_62(v, 0);
-		v = AMD_LS_CFG3_SET_UNKNOWN_56(v, 0);
-		v = AMD_LS_CFG3_SET_DIS_NC_FILLWITH_LTLI(v, 0);
-		/* XXX Possible policy option on B0+ only. */
-		v = AMD_LS_CFG3_SET_EN_SPEC_ST_FILL(v, 1);
-		v = AMD_LS_CFG3_SET_DIS_FAST_LD_BARRIER(v, 0);
-	} else if (genoa_ccx_set_undoc_fields) {
-		v = AMD_LS_CFG3_SET_UNKNOWN_62(v, 1);
-		v = AMD_LS_CFG3_SET_UNKNOWN_56(v, 1);
-		v = AMD_LS_CFG3_SET_DIS_NC_FILLWITH_LTLI(v, 1);
-		v = AMD_LS_CFG3_SET_EN_SPEC_ST_FILL(v, 0);
-	}
 	if (genoa_ccx_set_undoc_fields) {
 		v = AMD_LS_CFG3_SET_UNKNOWN_60(v, 1);
-		v = AMD_LS_CFG3_SET_UNKNOWN_57(v, 1);
+		v = AMD_LS_CFG3_SET_UNKNOWN_56(v, 1);
 	}
 	v = AMD_LS_CFG3_SET_DIS_SPEC_WC_NON_STRM_LD(v, 1);
-	v = AMD_LS_CFG3_SET_DIS_MAB_FULL_SLEEP(v, 1);
-	v = AMD_LS_CFG3_SET_DVM_SYNC_ONLY_ON_TLBI(v, 1);
+	v = AMD_LS_CFG3_SET_EN_SPEC_ST_FILL(v, 1);
 
 	wrmsr_and_test(MSR_AMD_LS_CFG3, v);
-
-	if (!chiprev_at_least(chiprev, X86_CHIPREV_AMD_GENOA_B0)) {
-		v = rdmsr(MSR_AMD_LS_CFG4);
-		v = AMD_LS_CFG4_SET_DIS_LIVE_LOCK_CNT_FST_BUSLOCK(v, 1);
-		v = AMD_LS_CFG4_SET_LIVE_LOCK_DET_FORCE_SBEX(v, 1);
-
-		wrmsr_and_test(MSR_AMD_LS_CFG4, v);
-	}
 }
 
 static void
 genoa_core_ic_init(void)
 {
 	uint64_t v;
-	x86_chiprev_t chiprev = cpuid_getchiprev(CPU);
 
 	v = rdmsr(MSR_AMD_IC_CFG);
-	if (genoa_ccx_set_undoc_fields) {
-		if (chiprev_at_least(chiprev, X86_CHIPREV_AMD_GENOA_B0)) {
-			v = AMD_IC_CFG_SET_UNKNOWN_48(v, 0);
-		} else {
-			v = AMD_IC_CFG_SET_UNKNOWN_48(v, 1);
-			v = AMD_IC_CFG_SET_UNKNOWN_8(v, 1);
-			v = AMD_IC_CFG_SET_UNKNOWN_7(v, 0);
-		}
-		v = AMD_IC_CFG_SET_UNKNOWN_53(v, 0);
-		v = AMD_IC_CFG_SET_UNKNOWN_52(v, 1);
-		v = AMD_IC_CFG_SET_UNKNOWN_51(v, 1);
-		v = AMD_IC_CFG_SET_UNKNOWN_50(v, 0);
-	}
+
 	/* XXX Possible policy option. */
 	v = AMD_IC_CFG_SET_OPCACHE_DIS(v, 0);
 
@@ -448,10 +393,12 @@ static void
 genoa_core_dc_init(void)
 {
 	uint64_t v;
-	x86_chiprev_t chiprev = cpuid_getchiprev(CPU);
 
 	/* XXX All of the prefetch controls may become policy options. */
 	v = rdmsr(MSR_AMD_DC_CFG);
+	v = AMD_DC_CFG_SET_EN_BURST_PFS(v, 1);
+	v = AMD_DC_CFG_SET_NUM_MABS_RSVD_HW_PF_L2(v, 3);
+	v = AMD_DC_CFG_SET_EN_PF_HIST_STREAM_HIT(v, 1);
 	v = AMD_DC_CFG_SET_DIS_REGION_HW_PF(v, 0);
 	v = AMD_DC_CFG_SET_DIS_STRIDE_HW_PF(v, 0);
 	v = AMD_DC_CFG_SET_DIS_STREAM_HW_PF(v, 0);
@@ -461,11 +408,7 @@ genoa_core_dc_init(void)
 	wrmsr_and_test(MSR_AMD_DC_CFG, v);
 
 	v = rdmsr(MSR_AMD_DC_CFG2);
-	if (chiprev_at_least(chiprev, X86_CHIPREV_AMD_GENOA_B0)) {
-		v = AMD_DC_CFG2_SET_DIS_DMB_STORE_LOCK(v, 0);
-	} else {
-		v = AMD_DC_CFG2_SET_DIS_DMB_STORE_LOCK(v, 1);
-	}
+	v = AMD_DC_CFG2_SET_DIS_DMB_STORE_LOCK(v, 0);
 	v = AMD_DC_CFG2_SET_DIS_SCB_NTA_L1(v, 1);
 
 	wrmsr_and_test(MSR_AMD_DC_CFG2, v);
@@ -485,40 +428,17 @@ genoa_core_tw_init(void)
 static void
 genoa_core_de_init(void)
 {
-	uint64_t v;
-	x86_chiprev_t chiprev = cpuid_getchiprev(CPU);
-
-	v = rdmsr(MSR_AMD_DE_CFG);
-	if (chiprev_matches(chiprev, X86_CHIPREV_AMD_GENOA_B0) &&
-	    genoa_ccx_set_undoc_fields) {
-		v = AMD_DE_CFG_SET_UNKNOWN_60(v, 0);
-		v = AMD_DE_CFG_SET_UNKNOWN_59(v, 0);
-	} else if (chiprev_at_least(chiprev, X86_CHIPREV_AMD_GENOA_B1) &&
-	    genoa_ccx_set_undoc_fields) {
-		v = AMD_DE_CFG_SET_UNKNOWN_48(v, 1);
-	} else if (genoa_ccx_set_undoc_fields) {
-		/* Older than B0 */
-		v = AMD_DE_CFG_SET_UNKNOWN_60(v, 1);
-		v = AMD_DE_CFG_SET_UNKNOWN_59(v, 1);
-	}
-	if (genoa_ccx_set_undoc_fields) {
-		v = AMD_DE_CFG_SET_UNKNOWN_33(v, 1);
-		v = AMD_DE_CFG_SET_UNKNOWN_32(v, 1);
-		v = AMD_DE_CFG_SET_UNKNOWN_28(v, 1);
-	}
-
-	wrmsr_and_test(MSR_AMD_DE_CFG, v);
+	/* Nothing to set in MSR_AMD_DE_CFG for Genoa */
 }
 
 static void
 genoa_core_l2_init(void)
 {
 	uint64_t v;
-	x86_chiprev_t chiprev = cpuid_getchiprev(CPU);
-	x86_uarchrev_t uarchrev = cpuid_getuarchrev(CPU);
 
 	v = rdmsr(MSR_AMD_L2_CFG);
-	v = AMD_L2_CFG_SET_DIS_HWA(v, 1);
+	v = AMD_L2_CFG_SET_UNKNOWN_52(v, 1);
+	v = AMD_L2_CFG_SET_DIS_HWA(v, 0);
 	v = AMD_L2_CFG_SET_DIS_L2_PF_LOW_ARB_PRIORITY(v, 1);
 	v = AMD_L2_CFG_SET_EXPLICIT_TAG_L3_PROBE_LOOKUP(v, 1);
 
@@ -532,10 +452,7 @@ genoa_core_l2_init(void)
 	wrmsr_and_test(MSR_AMD_CH_L2_PF_CFG, v);
 
 	v = rdmsr(MSR_AMD_CH_L2_CFG1);
-	if (chiprev_at_least(chiprev, X86_CHIPREV_AMD_GENOA_B0) &&
-	    uarchrev_at_least(uarchrev, X86_UARCHREV_AMD_ZEN3_B0)) {
-		v = AMD_CH_L2_CFG1_U_ZEN3_B0_SET_EN_BUSLOCK_IFETCH(v, 0);
-	}
+	v = AMD_CH_L2_CFG1_U_ZEN3_B0_SET_EN_BUSLOCK_IFETCH(v, 0);
 	v = AMD_CH_L2_CFG1_SET_EN_WCB_CONTEXT_DELAY(v, 1);
 	v = AMD_CH_L2_CFG1_SET_CBB_MASTER_EN(v, 0);
 	v = AMD_CH_L2_CFG1_SET_EN_PROBE_INTERRUPT(v, 1);
@@ -552,30 +469,15 @@ genoa_core_l2_init(void)
 	v = AMD_CH_L2_AA_CFG_SET_SCALE_REMOTE(v, AMD_CH_L2_AA_CFG_SCALE_MUL4);
 
 	wrmsr_and_test(MSR_AMD_CH_L2_AA_CFG, v);
-
-	v = rdmsr(MSR_AMD_CH_L2_AA_PAIR_CFG0);
-	v = AMD_CH_L2_AA_PAIR_CFG0_SET_SUPPRESS_DIFF_VICT(v, 1);
-
-	wrmsr_and_test(MSR_AMD_CH_L2_AA_PAIR_CFG0, v);
-
-	v = rdmsr(MSR_AMD_CH_L2_AA_PAIR_CFG1);
-	v = AMD_CH_L2_AA_PAIR_CFG1_SET_DEMAND_HIT_PF_RRIP(v, 0);
-	v = AMD_CH_L2_AA_PAIR_CFG1_SET_NOT_UNUSED_PF_RRIP_LVL_B4_L1V(v, 1);
-
-	wrmsr_and_test(MSR_AMD_CH_L2_AA_PAIR_CFG1, v);
 }
 
 static void
 genoa_ccx_l3_init(void)
 {
 	uint64_t v;
-	x86_chiprev_t chiprev = cpuid_getchiprev(CPU);
-	x86_uarchrev_t uarchrev = cpuid_getuarchrev(CPU);
 
 	v = rdmsr(MSR_AMD_CH_L3_CFG0);
-	if (uarchrev_at_least(uarchrev, X86_UARCHREV_AMD_ZEN3_B1)) {
-		v = AMD_CH_L3_CFG0_U_ZEN3_B1_SET_REPORT_SHARED_VIC(v, 1);
-	}
+	v = AMD_CH_L3_CFG0_U_ZEN3_B1_SET_REPORT_SHARED_VIC(v, 1);
 	v = AMD_CH_L3_CFG0_SET_REPORT_RESPONSIBLE_VIC(v, 1);
 
 	wrmsr_and_test(MSR_AMD_CH_L3_CFG0, v);
@@ -594,19 +496,10 @@ genoa_ccx_l3_init(void)
 	wrmsr_and_test(MSR_AMD_CH_L3_CFG1, v);
 
 	v = rdmsr(MSR_AMD_CH_L3_XI_CFG0);
-	if (chiprev_at_least(chiprev, X86_CHIPREV_AMD_GENOA_B0)) {
-		v = AMD_CH_L3_XI_CFG0_SET_SDR_REQ_BUSY_THRESH(v,
-		    AMD_CH_L3_XI_CFG0_SDR_REQ_BUSY_THRESH_767);
-	}
 	v = AMD_CH_L3_XI_CFG0_SET_SDP_REQ_WR_SIZED_COMP_EN(v, 1);
 	v = AMD_CH_L3_XI_CFG0_SET_SDP_REQ_VIC_BLK_COMP_EN(v, 1);
 	v = AMD_CH_L3_XI_CFG0_SET_SDP_REQ_WR_SIZED_ZERO_EN(v, 1);
 	v = AMD_CH_L3_XI_CFG0_SET_SDP_REQ_VIC_BLK_ZERO_EN(v, 1);
-	v = AMD_CH_L3_XI_CFG0_SET_SDR_HIT_SPEC_FEEDBACK_EN(v, 1);
-	v = AMD_CH_L3_XI_CFG0_SET_SDR_WASTE_THRESH(v,
-	    AMD_CH_L3_XI_CFG0_SDR_THRESH_191);
-	v = AMD_CH_L3_XI_CFG0_SET_SDR_SAMP_INTERVAL(v,
-	    AMD_CH_L3_XI_CFG0_SDR_SAMP_INTERVAL_16K);
 
 	wrmsr_and_test(MSR_AMD_CH_L3_XI_CFG0, v);
 }
@@ -615,41 +508,28 @@ static void
 genoa_core_undoc_init(void)
 {
 	uint64_t v;
-	x86_chiprev_t chiprev = cpuid_getchiprev(CPU);
+	x86_uarchrev_t uarchrev = cpuid_getuarchrev(CPU);
 
 	if (!genoa_ccx_set_undoc_regs)
 		return;
 
-	if (chiprev_at_least(chiprev, X86_CHIPREV_AMD_GENOA_B0)) {
-		v = rdmsr(MSR_AMD_UNKNOWN_C001_102C);
-		v = AMD_UNKNOWN_C001_102C_SET_UNKNOWN_58(v, 1);
-
-		wrmsr_and_test(MSR_AMD_UNKNOWN_C001_102C, v);
+	v = rdmsr(MSR_AMD_FP_CFG);
+	if (!uarchrev_at_least(uarchrev, X86_UARCHREV_AMD_ZEN4_B0)) {
+		v = AMD_FP_CFG_SET_UNKNOWN_52(v, 1);
+	}
+	if (uarchrev_matches(uarchrev, X86_UARCHREV_AMD_ZEN4_A0) ||
+	    uarchrev_matches(uarchrev, X86_UARCHREV_AMD_ZEN4_B0)) {
+		v = AMD_FP_CFG_SET_UNKNOWN_43(v, 1);
 	}
 
-	v = rdmsr(MSR_AMD_BP_CFG);
-	if (chiprev_at_least(chiprev, X86_CHIPREV_AMD_GENOA_B0)) {
-		v = AMD_BP_CFG_SET_UNKNOWN_14(v, 0);
-		v = AMD_BP_CFG_SET_UNKNOWN_6(v, 1);
-		v = AMD_BP_CFG_SET_UNKNOWN_1(v, 0);
-	} else {
-		v = AMD_BP_CFG_SET_UNKNOWN_14(v, 1);
-		v = AMD_BP_CFG_SET_UNKNOWN_6(v, 0);
-		v = AMD_BP_CFG_SET_UNKNOWN_1(v, 1);
-	}
-	/* Override B0 setting for UNKNOWN_5 */
-	if (chiprev_matches(chiprev, X86_CHIPREV_AMD_GENOA_A0) ||
-	    chiprev_at_least(chiprev, X86_CHIPREV_AMD_GENOA_B1)) {
-		v = AMD_BP_CFG_SET_UNKNOWN_5(v, 1);
-	}
-	v = AMD_BP_CFG_SET_UNKNOWN_4_2(v, 0);
-
-	wrmsr_and_test(MSR_AMD_BP_CFG, v);
+	wrmsr_and_test(MSR_AMD_FP_CFG, v);
 }
 
 static void
 genoa_core_dpm_init(void)
 {
+// XXX: fix retreiving weights from SMU first
+#if 0
 	const genoa_thread_t *thread = CPU->cpu_m.mcpu_hwthread;
 	const uint64_t *weights;
 	uint32_t nweights;
@@ -668,6 +548,7 @@ genoa_core_dpm_init(void)
 
 	cfg = AMD_DPM_CFG_SET_CFG_LOCKED(cfg, 1);
 	wrmsr_and_test(MSR_AMD_DPM_CFG, cfg);
+#endif
 }
 
 void
