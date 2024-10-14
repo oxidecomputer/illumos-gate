citrus-it/illumos-omnios

stlouis#629 IOAPIC programming for Genoa/Turin
Change-ID: Ib1eea04c64f31e3ff10cd5da17fd48f6248552ec

usr/src/uts/oxide/turin/turin_fabric.c
@@ -37,6 +37,62 @@
 #include <sys/io/turin/iohc.h>
 #include <sys/io/turin/iommu.h>
 #include <sys/io/turin/nbif_impl.h>
+#include <sys/io/turin/ioapic.h>
+
+/*
+ * The following table encodes the per-bridge IOAPIC initialization routing. We
+ * currently follow the recommendation of the PPR. Although IOAPIC instances on
+ * IOHUB[0,2] have 22 bridges and those on IOHUB[1,3] have 9, the configuration
+ * of the first 9 is common across both so we can get away with a single table.
+ */
+static const zen_ioapic_info_t turin_ioapic_routes[IOAPIC_NROUTES_IOAPIC0] = {
+	[0] = { .zii_group = 0x0, .zii_map = 0x0,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_ABCD },
+	[1] = { .zii_group = 0x1, .zii_map = 0x0,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_ABCD },
+	[2] = { .zii_group = 0x2, .zii_map = 0x0,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_ABCD },
+	[3] = { .zii_group = 0x3, .zii_map = 0x0,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_ABCD },
+	[4] = { .zii_group = 0x4, .zii_map = 0x0,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_ABCD },
+	[5] = { .zii_group = 0x5, .zii_map = 0x0,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_ABCD },
+	[6] = { .zii_group = 0x6, .zii_map = 0x0,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_ABCD },
+	[7] = { .zii_group = 0x6, .zii_map = 0x2,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_CDAB },
+	[8] = { .zii_group = 0x5, .zii_map = 0x0,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_CDAB },
+	[9] = { .zii_group = 0x4, .zii_map = 0x1,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_CDAB },
+	[10] = { .zii_group = 0x3, .zii_map = 0x1,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_CDAB },
+	[11] = { .zii_group = 0x2, .zii_map = 0x1,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_CDAB },
+	[12] = { .zii_group = 0x1, .zii_map = 0x1,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_CDAB },
+	[13] = { .zii_group = 0x0, .zii_map = 0x1,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_CDAB },
+	[14] = { .zii_group = 0x0, .zii_map = 0x1,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_BCDA },
+	[15] = { .zii_group = 0x1, .zii_map = 0x1,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_BCDA },
+	[16] = { .zii_group = 0x2, .zii_map = 0x1,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_BCDA },
+	[17] = { .zii_group = 0x3, .zii_map = 0x1,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_BCDA },
+	[18] = { .zii_group = 0x4, .zii_map = 0x2,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_BCDA },
+	[19] = { .zii_group = 0x5, .zii_map = 0x2,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_BCDA },
+	[20] = { .zii_group = 0x0, .zii_map = 0x0,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_ABCD },
+	[21] = { .zii_group = 0x1, .zii_map = 0x0,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_ABCD },
+};
+
+CTASSERT(ARRAY_SIZE(turin_ioapic_routes) == IOAPIC_NROUTES_IOAPIC0);
 
 const uint8_t turin_nbif_nfunc[] = {
 	[0] = TURIN_NBIF0_NFUNCS,
@@ -205,6 +261,9 @@ turin_ioms_reg(const zen_ioms_t *const ioms, const smn_reg_def_t def,
 	const uint8_t iunit = iomsno / 2 + (iomsno % 2) * 4;
 
 	switch (def.srd_unit) {
+	case SMN_UNIT_IOAPIC:
+		reg = turin_ioapic_smn_reg(iunit, def, reginst);
+		break;
 	case SMN_UNIT_IOHC:
 		reg = turin_iohc_smn_reg(iunit, def, reginst);
 		break;
@@ -505,6 +564,88 @@ turin_fabric_iohc_arbitration(zen_ioms_t *ioms)
 	val = IOHC_QOS_CTL_SET_VC2_PRI(val, 0);
 	val = IOHC_QOS_CTL_SET_VC1_PRI(val, 0);
 	val = IOHC_QOS_CTL_SET_VC0_PRI(val, 0);
+	zen_ioms_write(ioms, reg, val);
+}
+
+/*
+ * We need to initialize each IOAPIC as there is one per IOMS. First we
+ * initialize the interrupt routing table. This is used to mux the various
+ * legacy INTx interrupts and the bridge's interrupt to a given location. This
+ * follows from the PPR.
+ *
+ * After that we need to go through and program the feature register for the
+ * IOAPIC and its address. Because there is one IOAPIC per IOMS, one has to be
+ * elected the primary and the rest, secondary. This is done based on which
+ * IOMS has the FCH.
+ */
+void
+turin_fabric_ioapic(zen_ioms_t *ioms)
+{
+	smn_reg_t reg;
+	uint32_t val;
+	const uint_t nroutes = (ioms->zio_num % 2 == 0) ?
+	    IOAPIC_NROUTES_IOAPIC0 :
+	    IOAPIC_NROUTES_IOAPIC1;
+
+	for (uint_t i = 0; i < nroutes; i++) {
+		reg = turin_ioms_reg(ioms, D_IOAPIC_ROUTE, i);
+		val = zen_ioms_read(ioms, reg);
+
+		val = IOAPIC_ROUTE_SET_BRIDGE_MAP(val,
+		    turin_ioapic_routes[i].zii_map);
+		val = IOAPIC_ROUTE_SET_INTX_SWIZZLE(val,
+		    turin_ioapic_routes[i].zii_swiz);
+		val = IOAPIC_ROUTE_SET_INTX_GROUP(val,
+		    turin_ioapic_routes[i].zii_group);
+
+		zen_ioms_write(ioms, reg, val);
+	}
+
+	/*
+	 * The address registers are in the IOHC while the feature registers
+	 * are in the IOAPIC SMN space. To ensure that the other IOAPICs can't
+	 * be enabled with reset addresses, we instead lock them.
+	 * XXX Should we lock primary?
+	 */
+	reg = turin_ioms_reg(ioms, D_IOHC_IOAPIC_ADDR_HI, 0);
+	val = zen_ioms_read(ioms, reg);
+	if ((ioms->zio_flags & ZEN_IOMS_F_HAS_FCH) != 0) {
+		val = IOHC_IOAPIC_ADDR_HI_SET_ADDR(val,
+		    bitx64(ZEN_PHYSADDR_IOHC_IOAPIC, 47, 32));
+	} else {
+		val = IOHC_IOAPIC_ADDR_HI_SET_ADDR(val, 0);
+	}
+	zen_ioms_write(ioms, reg, val);
+
+	reg = turin_ioms_reg(ioms, D_IOHC_IOAPIC_ADDR_LO, 0);
+	val = zen_ioms_read(ioms, reg);
+	if ((ioms->zio_flags & ZEN_IOMS_F_HAS_FCH) != 0) {
+		val = IOHC_IOAPIC_ADDR_LO_SET_ADDR(val,
+		    bitx64(ZEN_PHYSADDR_IOHC_IOAPIC, 31, 8));
+		val = IOHC_IOAPIC_ADDR_LO_SET_LOCK(val, 0);
+		val = IOHC_IOAPIC_ADDR_LO_SET_EN(val, 1);
+	} else {
+		val = IOHC_IOAPIC_ADDR_LO_SET_ADDR(val, 0);
+		val = IOHC_IOAPIC_ADDR_LO_SET_LOCK(val, 1);
+		val = IOHC_IOAPIC_ADDR_LO_SET_EN(val, 0);
+	}
+	zen_ioms_write(ioms, reg, val);
+
+	/*
+	 * Every IOAPIC requires that we enable 8-bit addressing and that it be
+	 * able to generate interrupts to the FCH. The most important bit here
+	 * is the secondary bit which determines whether or not this IOAPIC is
+	 * subordinate to another.
+	 */
+	reg = turin_ioms_reg(ioms, D_IOAPIC_FEATURES, 0);
+	val = zen_ioms_read(ioms, reg);
+	if ((ioms->zio_flags & ZEN_IOMS_F_HAS_FCH) != 0) {
+		val = IOAPIC_FEATURES_SET_SECONDARY(val, 0);
+	} else {
+		val = IOAPIC_FEATURES_SET_SECONDARY(val, 1);
+	}
+	val = IOAPIC_FEATURES_SET_FCH(val, 1);
+	val = IOAPIC_FEATURES_SET_ID_EXT(val, 1);
 	zen_ioms_write(ioms, reg, val);
 }
 

oxidecomputer/illumos-gate

usr/src/uts/oxide/turin/turin_fabric.c
@@ -37,6 +37,63 @@
 #include <sys/io/turin/iohc.h>
 #include <sys/io/turin/iommu.h>
 #include <sys/io/turin/nbif_impl.h>
+#include <sys/io/turin/ioapic.h>
+
+/*
+ * The following table encodes the per-bridge IOAPIC initialization routing. We
+ * currently follow the recommendation of the PPR. Although IOAPIC instances on
+ * the larger IOHC instances have 22 bridges and the others have 9, the
+ * configuration of the first 9 is common across both so we can get away with a
+ * single table.
+ */
+static const zen_ioapic_info_t turin_ioapic_routes[IOAPIC_NROUTES_L] = {
+	[0] = { .zii_group = 0x0, .zii_map = 0x0,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_ABCD },
+	[1] = { .zii_group = 0x1, .zii_map = 0x0,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_ABCD },
+	[2] = { .zii_group = 0x2, .zii_map = 0x0,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_ABCD },
+	[3] = { .zii_group = 0x3, .zii_map = 0x0,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_ABCD },
+	[4] = { .zii_group = 0x4, .zii_map = 0x0,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_ABCD },
+	[5] = { .zii_group = 0x5, .zii_map = 0x0,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_ABCD },
+	[6] = { .zii_group = 0x6, .zii_map = 0x0,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_ABCD },
+	[7] = { .zii_group = 0x6, .zii_map = 0x2,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_CDAB },
+	[8] = { .zii_group = 0x5, .zii_map = 0x0,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_CDAB },
+	[9] = { .zii_group = 0x4, .zii_map = 0x1,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_CDAB },
+	[10] = { .zii_group = 0x3, .zii_map = 0x1,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_CDAB },
+	[11] = { .zii_group = 0x2, .zii_map = 0x1,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_CDAB },
+	[12] = { .zii_group = 0x1, .zii_map = 0x1,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_CDAB },
+	[13] = { .zii_group = 0x0, .zii_map = 0x1,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_CDAB },
+	[14] = { .zii_group = 0x0, .zii_map = 0x1,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_BCDA },
+	[15] = { .zii_group = 0x1, .zii_map = 0x1,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_BCDA },
+	[16] = { .zii_group = 0x2, .zii_map = 0x1,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_BCDA },
+	[17] = { .zii_group = 0x3, .zii_map = 0x1,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_BCDA },
+	[18] = { .zii_group = 0x4, .zii_map = 0x2,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_BCDA },
+	[19] = { .zii_group = 0x5, .zii_map = 0x2,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_BCDA },
+	[20] = { .zii_group = 0x0, .zii_map = 0x0,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_ABCD },
+	[21] = { .zii_group = 0x1, .zii_map = 0x0,
+	    .zii_swiz = IOAPIC_ROUTE_INTX_SWIZZLE_ABCD },
+};
+
+CTASSERT(ARRAY_SIZE(turin_ioapic_routes) == IOAPIC_NROUTES_L);
 
 const uint8_t turin_nbif_nfunc[] = {
 	[0] = TURIN_NBIF0_NFUNCS,
@@ -224,6 +281,9 @@ turin_ioms_reg(const zen_ioms_t *const ioms, const smn_reg_def_t def,
 	const uint8_t unit = iomsno / 2 + (iohcsz == IOHC_SZ_L ? 0 : 4);
 
 	switch (def.srd_unit) {
+	case SMN_UNIT_IOAPIC:
+		reg = turin_ioapic_smn_reg(unit, def, reginst);
+		break;
 	case SMN_UNIT_IOHC:
 		reg = turin_iohc_smn_reg(unit, def, reginst);
 		break;
@@ -535,6 +595,87 @@ turin_fabric_iohc_arbitration(zen_ioms_t *ioms)
 	val = IOHC_QOS_CTL_SET_VC2_PRI(val, 0);
 	val = IOHC_QOS_CTL_SET_VC1_PRI(val, 0);
 	val = IOHC_QOS_CTL_SET_VC0_PRI(val, 0);
+	zen_ioms_write(ioms, reg, val);
+}
+
+/*
+ * We need to initialize each IOAPIC as there is one per IOMS. First we
+ * initialize the interrupt routing table. This is used to mux the various
+ * legacy INTx interrupts and the bridge's interrupt to a given location. This
+ * follows from the PPR.
+ *
+ * After that we need to go through and program the feature register for the
+ * IOAPIC and its address. Because there is one IOAPIC per IOMS, one has to be
+ * elected the primary and the rest, secondary. This is done based on which
+ * IOMS has the FCH.
+ */
+void
+turin_fabric_ioapic(zen_ioms_t *ioms)
+{
+	smn_reg_t reg;
+	uint32_t val;
+	const uint_t nroutes = TURIN_IOHC_SZ(ioms->zio_num) == IOHC_SZ_L ?
+	    IOAPIC_NROUTES_L : IOAPIC_NROUTES_S;
+
+	for (uint_t i = 0; i < nroutes; i++) {
+		reg = turin_ioms_reg(ioms, D_IOAPIC_ROUTE, i);
+		val = zen_ioms_read(ioms, reg);
+
+		val = IOAPIC_ROUTE_SET_BRIDGE_MAP(val,
+		    turin_ioapic_routes[i].zii_map);
+		val = IOAPIC_ROUTE_SET_INTX_SWIZZLE(val,
+		    turin_ioapic_routes[i].zii_swiz);
+		val = IOAPIC_ROUTE_SET_INTX_GROUP(val,
+		    turin_ioapic_routes[i].zii_group);
+
+		zen_ioms_write(ioms, reg, val);
+	}
+
+	/*
+	 * The address registers are in the IOHC while the feature registers
+	 * are in the IOAPIC SMN space. To ensure that the other IOAPICs can't
+	 * be enabled with reset addresses, we instead lock them.
+	 * XXX Should we lock primary?
+	 */
+	reg = turin_ioms_reg(ioms, D_IOHC_IOAPIC_ADDR_HI, 0);
+	val = zen_ioms_read(ioms, reg);
+	if ((ioms->zio_flags & ZEN_IOMS_F_HAS_FCH) != 0) {
+		val = IOHC_IOAPIC_ADDR_HI_SET_ADDR(val,
+		    bitx64(ZEN_PHYSADDR_IOHC_IOAPIC, 47, 32));
+	} else {
+		val = IOHC_IOAPIC_ADDR_HI_SET_ADDR(val, 0);
+	}
+	zen_ioms_write(ioms, reg, val);
+
+	reg = turin_ioms_reg(ioms, D_IOHC_IOAPIC_ADDR_LO, 0);
+	val = zen_ioms_read(ioms, reg);
+	if ((ioms->zio_flags & ZEN_IOMS_F_HAS_FCH) != 0) {
+		val = IOHC_IOAPIC_ADDR_LO_SET_ADDR(val,
+		    bitx64(ZEN_PHYSADDR_IOHC_IOAPIC, 31, 8));
+		val = IOHC_IOAPIC_ADDR_LO_SET_LOCK(val, 0);
+		val = IOHC_IOAPIC_ADDR_LO_SET_EN(val, 1);
+	} else {
+		val = IOHC_IOAPIC_ADDR_LO_SET_ADDR(val, 0);
+		val = IOHC_IOAPIC_ADDR_LO_SET_LOCK(val, 1);
+		val = IOHC_IOAPIC_ADDR_LO_SET_EN(val, 0);
+	}
+	zen_ioms_write(ioms, reg, val);
+
+	/*
+	 * Every IOAPIC requires that we enable 8-bit addressing and that it be
+	 * able to generate interrupts to the FCH. The most important bit here
+	 * is the secondary bit which determines whether or not this IOAPIC is
+	 * subordinate to another.
+	 */
+	reg = turin_ioms_reg(ioms, D_IOAPIC_FEATURES, 0);
+	val = zen_ioms_read(ioms, reg);
+	if ((ioms->zio_flags & ZEN_IOMS_F_HAS_FCH) != 0) {
+		val = IOAPIC_FEATURES_SET_SECONDARY(val, 0);
+	} else {
+		val = IOAPIC_FEATURES_SET_SECONDARY(val, 1);
+	}
+	val = IOAPIC_FEATURES_SET_FCH(val, 1);
+	val = IOAPIC_FEATURES_SET_ID_EXT(val, 1);
 	zen_ioms_write(ioms, reg, val);
 }
 