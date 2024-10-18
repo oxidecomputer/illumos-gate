9195877dac46c0232a99ecc5aaf756aa9516af56
Dan Cross <cross@oxidecomputer.com>
2024-10-18 21:37:20 +0000

    WIP: hacks
    Change-Id: I4a9dd9a4adeba5630a89aba48e532104edda3cf4

--- usr/src/uts/oxide/zen/zen_fabric.c
+++ usr/src/uts/oxide/zen/zen_fabric.c
@@ -1482,7 +1482,7 @@ zen_fabric_topo_init(void)
 		    nthreads, NCPU);
 		nthreads = NCPU;
 	}
-	boot_max_ncpus = max_ncpus = boot_ncpus = nthreads;
+	boot_max_ncpus = max_ncpus = boot_ncpus = nthreads = 1;
 }
 
 static int
