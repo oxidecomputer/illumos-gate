citrus-it/illumos-omnios

stlouis#752 untangle IOMS/IOHC/NBIO numbering across platforms stlouis#753 represent NBIOs in the fabric
Change-ID: Ia8ebef2b313a8673303ec1b6d7c682535a1de292

usr/src/uts/oxide/milan/milan_platform.c
@@ -97,6 +97,7 @@ static const zen_fabric_ops_t milan_fabric_ops = {
 	.zfo_nbif_init = milan_fabric_nbif_init,
 	.zfo_nbif_dev_straps = milan_fabric_nbif_dev_straps,
 	.zfo_nbif_bridges = milan_fabric_nbif_bridges,
+	.zfo_ioms_nbio_num = milan_fabric_ioms_nbio_num,
 	.zfo_pcie = milan_fabric_pcie,
 	.zfo_pcie_port_is_trained = milan_fabric_pcie_port_is_trained,
 	.zfo_pcie_port_hide_bridge = milan_fabric_hide_bridge,
@@ -122,7 +123,7 @@ static const zen_fabric_ops_t milan_fabric_ops = {
 	.zfo_get_dxio_fw_version = milan_get_dxio_fw_version,
 	.zfo_report_dxio_fw_version = milan_report_dxio_fw_version,
 
-	.zfo_ioms_n_pcie_cores = milan_ioms_n_pcie_cores,
+	.zfo_iohc_n_pcie_cores = milan_iohc_n_pcie_cores,
 	.zfo_pcie_core_n_ports = milan_pcie_core_n_ports,
 	.zfo_pcie_core_info = milan_pcie_core_info,
 	.zfo_pcie_port_info = milan_pcie_port_info,
@@ -160,7 +161,7 @@ const zen_platform_t milan_platform = {
 			.zssa_arg4 = D_MILAN_SMU_RPC_ARG4,
 			.zssa_arg5 = D_MILAN_SMU_RPC_ARG5,
 		},
-		.zpc_nnbif = MILAN_IOMS_MAX_NBIF,
+		.zpc_nnbif = MILAN_IOHC_MAX_NBIF,
 		.zpc_nbif_nfunc = milan_nbif_nfunc,
 		.zpc_nbif_data = milan_nbif_data,
 		.zpc_pcie_core0_unitid = MILAN_PCIE_CORE0_UNITID,

oxidecomputer/illumos-gate

usr/src/uts/oxide/milan/milan_platform.c
@@ -97,6 +97,7 @@ static const zen_fabric_ops_t milan_fabric_ops = {
 	.zfo_nbif_init = milan_fabric_nbif_init,
 	.zfo_nbif_dev_straps = milan_fabric_nbif_dev_straps,
 	.zfo_nbif_bridges = milan_fabric_nbif_bridges,
+	.zfo_ioms_nbio_num = milan_fabric_ioms_nbio_num,
 	.zfo_pcie = milan_fabric_pcie,
 	.zfo_pcie_port_is_trained = milan_fabric_pcie_port_is_trained,
 	.zfo_pcie_port_hide_bridge = milan_fabric_hide_bridge,
@@ -122,7 +123,7 @@ static const zen_fabric_ops_t milan_fabric_ops = {
 	.zfo_get_dxio_fw_version = milan_get_dxio_fw_version,
 	.zfo_report_dxio_fw_version = milan_report_dxio_fw_version,
 
-	.zfo_ioms_n_pcie_cores = milan_ioms_n_pcie_cores,
+	.zfo_iohc_n_pcie_cores = milan_iohc_n_pcie_cores,
 	.zfo_pcie_core_n_ports = milan_pcie_core_n_ports,
 	.zfo_pcie_core_info = milan_pcie_core_info,
 	.zfo_pcie_port_info = milan_pcie_port_info,
@@ -160,7 +161,7 @@ const zen_platform_t milan_platform = {
 			.zssa_arg4 = D_MILAN_SMU_RPC_ARG4,
 			.zssa_arg5 = D_MILAN_SMU_RPC_ARG5,
 		},
-		.zpc_nnbif = MILAN_IOMS_MAX_NBIF,
+		.zpc_nnbif = MILAN_NBIO_MAX_NBIF,
 		.zpc_nbif_nfunc = milan_nbif_nfunc,
 		.zpc_nbif_data = milan_nbif_data,
 		.zpc_pcie_core0_unitid = MILAN_PCIE_CORE0_UNITID,