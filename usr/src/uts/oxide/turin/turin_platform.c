a1aa47bb23469b330bb6c94d0dda82f5978757eb
Andy Fiddaman <illumos@fiddaman.net>
2025-07-29 11:42:24 +0000

    Initialise Turin SMU features earlier

--- usr/src/uts/oxide/turin/turin_platform.c
+++ usr/src/uts/oxide/turin/turin_platform.c
@@ -87,12 +87,7 @@ static const zen_fabric_ops_t turin_fabric_ops = {
 	.zfo_set_mpio_global_config = turin_set_mpio_global_config,
 	.zfo_smu_pptable_init = turin_fabric_smu_pptable_init,
 	.zfo_smu_pptable_post = turin_fabric_smu_pptable_post,
-	/*
-	 * We currently initialize SMU features late on Turin, after APs are
-	 * running. Doing it earlier can cause a fault state where the CPU
-	 * frequency collapses to Fmin and stays there.
-	 */
-	.zfo_smu_misc_late_init = turin_smu_features_init,
+	.zfo_smu_misc_init = turin_smu_features_init,
 	.zfo_nbio_init = turin_fabric_nbio_init,
 	.zfo_ioms_init = turin_fabric_ioms_init,
 	.zfo_misc_late_init = turin_fabric_misc_late_init,
