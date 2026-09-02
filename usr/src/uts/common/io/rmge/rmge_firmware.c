681f55b2cc51ef72425d5bcc6697ebd14f1ab7ff
Daniel Levin <daniel.levin@oxidecomputer.com>
2026-09-02 05:57:17 +0000

    Want: multi-gig realtek driver
    Change-Id: I1589aced235bf9e0d8b1c04c913a711dde89a883

---
+++ usr/src/uts/common/io/rmge/rmge_firmware.c
@@ -0,0 +1,13 @@
+/*
+ * This file and its contents are supplied under the terms of the
+ * Common Development and Distribution License ("CDDL"), version 1.0.
+ * You may only use this file in accordance with the terms of version
+ * 1.0 of the CDDL.
+ *
+ * Copyright 2026 Oxide Computer Company
+ */
+
+void
+rmge_fw_stub()
+{
+}
