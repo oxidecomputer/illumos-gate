8ca2dd89b845ef52c732d651c1a12def73c66c4f
Daniel Levin <daniel.levin@oxidecomputer.com>
2026-09-04 13:51:12 +0000

    cuddle
    Change-Id: I3528361b8732a27d4954764b0b3fd29b5e805304

--- usr/src/uts/common/io/rmge/rmge_hw.c
+++ usr/src/uts/common/io/rmge/rmge_hw.c
@@ -1,3 +1,12 @@
+/*
+ * This file and its contents are supplied under the terms of the
+ * Common Development and Distribution License ("CDDL"), version 1.0.
+ * You may only use this file in accordance with the terms of version
+ * 1.0 of the CDDL.
+ *
+ * Copyright 2026 Oxide Computer Company
+ */
+
 #include "rmge.h"
 
 static uint32_t
